3b7327adb1e1048f72c502bcf6f693f7b3505f68
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2023-01-29 11:03:15 +0100

    fix server.mjs

--- server.mjs
+++ server.mjs
@@ -2,12 +2,12 @@ import path from 'path';
 import jsonServer from 'json-server';
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join('dist', 'db', 'app.json');
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
 });
-const port = process.env.PORT || 3131;
+const port = process.env.PORT || 3131; 
 
 server.use(middlewares);
 server.use(router);
